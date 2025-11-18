101435701dc592835222043ada6745ad3d68f8f3
Prarang <prarang.1125@gmail.com>
2025-11-18 06:32:57 +0000

    ols

--- app/config/settings.py
+++ app/config/settings.py
@@ -13,6 +13,11 @@ class Settings(BaseSettings):
     db_user: str
     db_password: str
     db_name: str
+    cirus_db_host: str | None = None
+    cirus_db_port: int | None = None
+    cirus_db_user: str | None = None
+    cirus_db_password: str | None = None
+    cirus_db_name: str | None = None
 
     class Config:
         env_file = ".env"
